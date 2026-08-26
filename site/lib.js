b6528f0073403d593d92c6c87382fe12923e6444
Pukar Hamal <15203448+pchamal@users.noreply.github.com>
2026-08-26 22:56:05 +0000

    file the next batch of public trust centers

--- site/lib.js
+++ site/lib.js
@@ -2,7 +2,7 @@
 
 export const GATE_KEY = "ot_human_v1";
 export const GATE_MS = 30 * 60 * 1000;
-export const DATA_V = "2026-08-26T21:51:30Z";
+export const DATA_V = "2026-08-26T22:55:06Z";
 export const FILE_KEYS = ["page", "marks", "dpa", "subprocessors", "years"];
 export const FILE_LABELS = {
   page: "page",
